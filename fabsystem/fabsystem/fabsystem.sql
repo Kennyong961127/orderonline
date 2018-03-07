33a2f8d0458386c0fd037834d29e06bef991f7c7
Kennyong961127 <37101678+Kennyong961127@users.noreply.github.com>
2018-03-07 16:29:03 +0800

    Add files via upload

---
+++ fabsystem/fabsystem/fabsystem.sql
@@ -0,0 +1,298 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 07, 2018 at 09:11 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fabsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `customer_name` varchar(50) NOT NULL,
+  `customer_address` varchar(100) NOT NULL,
+  `customer_contact` int(50) NOT NULL,
+  `order_id` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foods`
+--
+
+CREATE TABLE `foods` (
+  `food_id` int(11) NOT NULL,
+  `food_name` varchar(50) NOT NULL,
+  `food_description` varchar(50) NOT NULL,
+  `food_price` decimal(6,2) NOT NULL,
+  `subcategory_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `foods`
+--
+
+INSERT INTO `foods` (`food_id`, `food_name`, `food_description`, `food_price`, `subcategory_id`) VALUES
+(1, 'Curry Chicken Rice', '咖喱鸡饭', '8.90', 1),
+(2, 'Mamee Chicken Rice', '妈蜜鸡丁饭', '8.90', 1),
+(3, 'Mamee Fish Rice', '妈蜜鱼片饭', '8.90', 2),
+(4, 'Mamee Prawn Rice', '妈蜜虾仁饭', '9.90', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_categories`
+--
+
+CREATE TABLE `food_categories` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `food_categories`
+--
+
+INSERT INTO `food_categories` (`category_id`, `category_name`) VALUES
+(1, 'Rice'),
+(2, 'Indo'),
+(3, 'Local'),
+(4, 'Snack'),
+(5, 'Soup'),
+(6, 'Drink');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_subcategories`
+--
+
+CREATE TABLE `food_subcategories` (
+  `subcategory_id` int(11) NOT NULL,
+  `subcategory_name` varchar(50) NOT NULL,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `food_subcategories`
+--
+
+INSERT INTO `food_subcategories` (`subcategory_id`, `subcategory_name`, `category_id`) VALUES
+(1, 'Chicken', 1),
+(2, 'Fish', 1),
+(3, 'Prawn', 1),
+(4, 'Sotong', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `order_time` date NOT NULL,
+  `delivery_time` date NOT NULL,
+  `order_gross` decimal(6,2) NOT NULL,
+  `order_discount` decimal(6,2) NOT NULL,
+  `order_delivery` decimal(6,2) NOT NULL,
+  `rider_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `riders`
+--
+
+CREATE TABLE `riders` (
+  `rider_id` int(11) NOT NULL,
+  `rider_name` varchar(50) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `rider_earned` decimal(6,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_first` varchar(50) NOT NULL,
+  `user_last` varchar(50) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_pwd` varchar(50) NOT NULL,
+  `user_hash` varchar(50) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_first`, `user_last`, `user_email`, `user_pwd`, `user_hash`, `active`) VALUES
+(3, 'Kim', 'kaka', 'lalala@gmail.com', '$2y$10$/OCTwztehDIHFlgFxXNIpuXdwzVG3Rl6nxs/vW8fzoM', '3871bd64012152bfb53fdf04b401193f', 0),
+(14, 'Kenny', 'Ong', 'kenny961127@hotmail.co.uk', '$2y$10$dxVjPK.l95wFOOOzQJOcneYGDT0o7CZgcRZ8o8z1o5b', 'a8c88a0055f636e4a163a5e3d16adab7', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `foods`
+--
+ALTER TABLE `foods`
+  ADD PRIMARY KEY (`food_id`),
+  ADD KEY `category_id` (`subcategory_id`);
+
+--
+-- Indexes for table `food_categories`
+--
+ALTER TABLE `food_categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `food_subcategories`
+--
+ALTER TABLE `food_subcategories`
+  ADD PRIMARY KEY (`subcategory_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `rider_id` (`rider_id`);
+
+--
+-- Indexes for table `riders`
+--
+ALTER TABLE `riders`
+  ADD PRIMARY KEY (`rider_id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `foods`
+--
+ALTER TABLE `foods`
+  MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `food_categories`
+--
+ALTER TABLE `food_categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `food_subcategories`
+--
+ALTER TABLE `food_subcategories`
+  MODIFY `subcategory_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `riders`
+--
+ALTER TABLE `riders`
+  MODIFY `rider_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `customers`
+--
+ALTER TABLE `customers`
+  ADD CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
+
+--
+-- Constraints for table `foods`
+--
+ALTER TABLE `foods`
+  ADD CONSTRAINT `foods_ibfk_1` FOREIGN KEY (`subcategory_id`) REFERENCES `food_subcategories` (`subcategory_id`);
+
+--
+-- Constraints for table `food_subcategories`
+--
+ALTER TABLE `food_subcategories`
+  ADD CONSTRAINT `food_subcategories_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `food_categories` (`category_id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`rider_id`) REFERENCES `riders` (`rider_id`);
+
+--
+-- Constraints for table `riders`
+--
+ALTER TABLE `riders`
+  ADD CONSTRAINT `riders_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ fabsystem/fabsystem/fabsystem/helper/loginsystem/reset_password.php
@@ -0,0 +1,34 @@
+<?php
+/* Password reset process, updates database with new user password */
+require 'db.php';
+session_start();
+
+// Make sure the form is being submitted with method="post"
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    // Make sure the two passwords match
+    if ( $_POST['newpassword'] == $_POST['confirmpassword'] ) {
+
+        $new_password = password_hash($_POST['newpassword'], PASSWORD_BCRYPT);
+
+        // We get $_POST['email'] and $_POST['hash'] from the hidden input field of reset.php form
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $hash = mysqli_real_escape_string($conn, $_POST['hash']);
+
+        $sql = "UPDATE users SET user_password='$new_password', hash='$hash' WHERE user_email='$email'";
+
+        if ( mysqli_query($conn,$sql) ) {
+
+        $_SESSION['message'] = "Your password has been reset successfully!";
+        header("location: success.php");
+
+        }
+
+    }
+    else {
+        $_SESSION['message'] = "Two passwords you entered don't match, try again!";
+        header("location: error.php");
+    }
+
+}
+?>
